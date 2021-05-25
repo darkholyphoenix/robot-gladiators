ceb228bacb35ecfcef67168fdf71135a7dae5f38
James Bowen (J.D.) <j.bowen1821@gmail.com>
2021-05-25 06:02:45 -0500

    feature/shop-input addition

--- assets/js/game.js
+++ assets/js/game.js
@@ -177,23 +177,22 @@ var shop = function() {
   );
 
   // use switch case to carry out action
+  shopOptionPrompt = parseInt(shopOptionPrompt);
   switch (shopOptionPrompt) {
-    case 'REFILL':
-    case 'refill':
+    case 1:
       playerInfo.refillHealth();
       break;
-    case 'UPGRADE':
-    case 'upgrade':
+    case 2:
       playerInfo.upgradeAttack();
       break;
-    case 'LEAVE':
-    case 'leave':
-      window.alert('Leaving the store.');
-
-      // do nothing, so function will end
+    case 3:
+      window.alert("Leaving the store.");
       break;
     default:
-      window.alert('You did not pick a valid option. Try again.');
+      window.alert("You did not pick a valid option. Try again.");
+      shop();
+      break;
+  
 
       // call shop() again to force player to pick a valid option
       shop();
